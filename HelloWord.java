7eb604e8b0c08cdec6c8a1e6d0162cdc2358c56c
jhugues <jhugues.coindin@gmail.com>
2019-11-05 15:26:34 +0100

    1er

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,5 @@
-public	class HelloWorld	{
+public	class HelloWord{
 		public static void main(String[] args){
-			system.out.println(Hello jenkins!");
+			System.out.println("Hello jenkins!");
 		}
 }
